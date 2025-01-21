e3c08c933d5a1d34b03b2e86333e096705def008
Muhlis Abdi <enterpriseabdi@gmail.com>
2025-01-21 09:33:48 +0800

    Revert "Update Updater.vue"
    This reverts commit 6fa5f99752becf8be3f27c079c7b463f7c234925.

--- resources/js/components/Updater.vue
+++ resources/js/components/Updater.vue
@@ -1,55 +1,25 @@
 <template>
   <div class="relative">
-    <Button variant="action" icon="arrow-path" @click="update"> </Button>
-  </div>
-  <Modal v-if="showModal" size="'2xl'" role="alertdialog">
-    <div
-      class="mx-auto bg-white dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden"
+    <Button
+      variant="action"
+      icon="arrow-path"
+      @click="update"
     >
-      <slot>
-        <ModalHeader v-text="Update Log" />
-        <div class="py-3 px-8">
-          <p>{{ output }}</p>
-        </div>
-      </slot>
-
-      <ModalFooter>
-        <div class="ml-auto">
-          <Button
-            variant="link"
-            state="mellow"
-            @click="showModal = false"
-            class="mr-3"
-            dusk="close-modal-button"
-          >
-            Close
-          </Button>
-        </div>
-      </ModalFooter>
-    </div>
-  </Modal>
+    </Button>
+  </div>
 </template>
 
 <script>
-import { Button } from "laravel-nova-ui";
+import { Button } from 'laravel-nova-ui'
 export default {
   name: "Updater",
   components: { Button },
-  data() {
-    return {
-      showModal: false,
-      output: "",
-    };
-  },
-  methods: {
+  methods:{
     update() {
-      Nova.request()
-        .post("/nova-vendor/updater/update")
-        .then((response) => {
-          this.output = response.data.output;
-          this.showModal = true;
+        Nova.request().post('/nova-vendor/updater/update').then(response => {
+          alert(response.data.status);
         });
     },
-  },
-};
+  }
+}
 </script>
