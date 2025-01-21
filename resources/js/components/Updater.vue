b3032c270e3deda89d7fd0ad750133f47e74bd08
Muhlis Abdi <enterpriseabdi@gmail.com>
2025-01-21 09:57:09 +0800

    1.0.3

--- resources/js/components/Updater.vue
+++ resources/js/components/Updater.vue
@@ -2,7 +2,7 @@
   <div class="relative">
     <Button
       variant="action"
-      icon="arrow-path"
+      :icon="buttonIcon"
       @click="update"
     >
     </Button>
@@ -14,10 +14,17 @@ import { Button } from 'laravel-nova-ui'
 export default {
   name: "Updater",
   components: { Button },
+    data() {
+    return {
+      buttonIcon: 'arrow-path'
+    }
+  },
   methods:{
     update() {
+      this.buttonIcon = 'play-circle';
         Nova.request().post('/nova-vendor/updater/update').then(response => {
           alert(response.data.status);
+          this.buttonIcon = 'arrow-path';
         });
     },
   }
